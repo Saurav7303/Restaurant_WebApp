6aa3007a4bc409c1cb12782a330598da14386b73
Navin <navin@gmail.com>
2025-04-26 09:49:28 +0530

    files added

--- Restaurant_Project/Restaurant_Project/settings.py
+++ Restaurant_Project/Restaurant_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$xv^u%okrde#t8wdk&^sq!52*qbcw($8%%)w!--%$_n(pk(a-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
